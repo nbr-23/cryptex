c9003bcff5bf2f404fbd33164168f95db3717f67
bella <the-nbr23@protonmail.com>
2020-04-04 17:03:38 +0200

    + fixtures for articles

---
+++ src/DataFixtures/ArticlesFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Article; 
+
+class ArticlesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for($i = 1; $i <= 10; $i++){
+            $article = new Article();
+            $article->setTitle("Titre de l'\article n°$i")
+                    ->setContent("<p>Contenue de l'\article n°$i")
+                    ->setImage("http://placehold.it/350x150")
+                    ->setCreatedAt(new \DateTime());
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
