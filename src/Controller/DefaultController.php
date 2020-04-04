1819e389a37f6047bd0c4a17bc04d78e1d44c295
bella <the-nbr23@protonmail.com>
2020-04-04 18:40:28 +0200

    + refactor functions in DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
 
         $articles = $repo->findAll();
         return $this->render('default/index.html.twig', [
@@ -33,10 +33,9 @@ class DefaultController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id)
+    public function show(Article $article)
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-        $article = $repo->find($id);
+       
         return $this->render('default/show.html.twig', [
             'article' => $article
         ]);
