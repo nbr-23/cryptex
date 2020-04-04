84e8485e7aa83c25556457f18c6051687e46efbd
bella <the-nbr23@protonmail.com>
2020-04-04 17:23:49 +0200

    modification of article

--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -14,8 +14,8 @@ class ArticlesFixtures extends Fixture
         // $manager->persist($product);
         for($i = 1; $i <= 10; $i++){
             $article = new Article();
-            $article->setTitle("Titre de l'\article n°$i")
-                    ->setContent("<p>Contenue de l'\article n°$i")
+            $article->setTitle("Titre de l'article n°$i")
+                    ->setContent("<p>Contenue de l'article n°$i")
                     ->setImage("http://placehold.it/350x150")
                     ->setCreatedAt(new \DateTime());
             $manager->persist($article);
